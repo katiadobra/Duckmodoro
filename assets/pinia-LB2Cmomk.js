3745e031bc5e6484f833c20d245bcab0b3502db2
katiadobra <katiadobra@users.noreply.github.com>
2025-05-25 13:19:57 +0000

    deploy: 4ead2eac2ab9c8b0952d29277dcb5f802fbab410

--- assets/pinia-LB2Cmomk.js
+++
@@ -1,1 +0,0 @@
-import{c as i}from"./pinia-ifK-9CeS.js";import"./index-Brz0LGU7.js";const o=async({app:a})=>{a.use(i())};export{o as default};
---
+++ assets/pinia-Np0sgKAQ.js
@@ -0,0 +1,1 @@
+import{c as i}from"./pinia-CW4-lDCY.js";import"./index-CtTdL9cw.js";const o=async({app:a})=>{a.use(i())};export{o as default};
